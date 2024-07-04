0b459443d4bf457bd06694aa7f96f9d048041659
Jhansi M <jhnsjaan@gmail.com>
2024-07-04 13:14:04 +0530

    first commit

---
+++ MapingUsersToGetUsername/index.js
@@ -0,0 +1,21 @@
+//1.Write a code to get array of names from given array of users.
+
+const users=[
+    {
+        id:1,
+        name:"Jack",
+        isActive:false
+    },
+    {
+        id:2,
+        name:"John",
+        isActive:false
+    },
+    {
+        id:3,
+        name:"James",
+        isActive:true
+    },
+
+]
+
