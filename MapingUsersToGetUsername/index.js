2aa53644493aba51e8cd22c3cd4cdc2b111c15f0
Jhansi M <jhnsjaan@gmail.com>
2024-07-05 10:41:16 +0530

    updated code

--- MapingUsersToGetUsername/index.js
+++ MapingUsersToGetUsername/index.js
@@ -1,20 +1,27 @@
 //1.Write a code to get array of names from given array of users.
+//2.if it is active get username
+//3.Sort users by age descending
+
+
 
 const users=[
     {
         id:1,
         name:"Jack",
-        isActive:false
+        isActive:false,
+        age:25
     },
     {
         id:2,
         name:"John",
-        isActive:true
+        isActive:true,
+        age:28
     },
     {
         id:3,
         name:"James",
-        isActive:true
+        isActive:true,
+        age:20
     },
 
 ]
@@ -41,8 +48,14 @@ const users=[
 const names=users.map((user=>user.name))
 console.log(names)
 
-//if it is active get username
 
-const getActiveUsername=users.filter((user)=>user.isActive).map((user)=>user.name)
+
+// const sorteddata=users.sort((user1,user2)=>user1.age < user2.age ? 1 : -1)
+// console.log(sorteddata)
+
+
+const getActiveUsername=users.sort((user1,user2)=>user1.age<user2.age ? 1 : -1).filter((user)=>user.isActive).map((user)=>user.name)
 console.log(getActiveUsername)
+
+
 
