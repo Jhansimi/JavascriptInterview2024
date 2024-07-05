c30ac25f161e18f691e623e8b3ce7971ec1f2079
Jhansi M <jhnsjaan@gmail.com>
2024-07-05 10:54:35 +0530

    added difference b/w undefind and null

---
+++ NullAndUndefined/index.js
@@ -0,0 +1,17 @@
+let var1;
+console.log(var1)
+console.log(typeof(var1))
+
+//output: undefined
+//output: undefined
+
+/*...................................*/
+
+
+
+let var2=null;
+console.log(var2)
+console.log(typeof(var2))
+
+//output: null
+//output: object
